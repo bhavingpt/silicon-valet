54f5523f2c2bd748cf25602e9ebec36559a891db
Nicolas Carmont <nickcarmont8@gmail.com>
2018-11-03 23:55:12 -0700

    Wired up Firebase database

--- SiliconValetBetter/Pods/Target Support Files/Pods-SiliconValetBetter/Pods-SiliconValetBetter-frameworks.sh
+++ SiliconValetBetter/Pods/Target Support Files/Pods-SiliconValetBetter/Pods-SiliconValetBetter-frameworks.sh
@@ -144,11 +144,17 @@ strip_invalid_archs() {
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/BiSON/BiSON.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/GoogleUtilities/GoogleUtilities.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/SmartDeviceLink/SmartDeviceLink.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/leveldb-library/leveldb.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/nanopb/nanopb.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/BiSON/BiSON.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/GoogleUtilities/GoogleUtilities.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/SmartDeviceLink/SmartDeviceLink.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/leveldb-library/leveldb.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/nanopb/nanopb.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
